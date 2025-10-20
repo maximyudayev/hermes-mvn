8ecdc3a2e4d9d6041772ab5a582e5dfc99793c2d
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-10-20 21:51:25 +0200

    feat: runnable namespace package

---
+++ examples/mvn.bat
@@ -0,0 +1,1 @@
+call hermes-cli -o .\ --config_file mvn.yml --experiment project=Test type=MVN trial=0
---
+++ examples/mvn.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hermes-cli -o ./ --config_file mvn.yml --experiment project=Test type=MVN trial=0
